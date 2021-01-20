1568744125fb4dae0065757e7a66d782a39cd481
tomaszzakiwnu1998 <m.tomaszewski11@interia.pl>
2021-01-21 00:13:55 +0100

    zaczynam javadoc

--- src/biblioteka/Person.java
+++ src/biblioteka/Person.java
@@ -1,12 +1,24 @@
 package biblioteka;
 
+/**
+ * Klasa reprezntująca dowolnego klienta systemu, będzie rozszerzana poprzez inne klasy
+ */
 public class Person {
+    /**
+     * Imie danego klienta systemu
+     */
     String name;
+    /**
+     * Nazwisko danego klienta systemu
+     */
     String surname;
-    Person(){
 
-    }
 
+    /**
+     * Konstruktor dwuargumentowy tworzący obiekt klasy @Person
+     * @param name Imię podawnane przy tworzeniu nowego konta klienta systemu
+     * @param surname Nazwisko podawnane przy tworzeniu nowego konta klienta systemu
+     */
     Person(String name, String surname){
         this.name=name;
         this.surname=surname;
